6fb1999e825b1235cded8b5bf4a8613ab0d95c35
nikkaushal <nikkaushal@gmial.com>
2026-03-10 21:06:47 -0400

    frontend sh file push

---
+++ common.sh
@@ -0,0 +1,25 @@
+app_prereq() {
+  id appuser &>/dev/null
+  if [ $? -ne 0 ]; then
+    useradd -r -s /bin/false appuser
+  fi
+  mkdir -p /app
+  curl -L -o /tmp/${service_name}.tar.gz https://raw.githubusercontent.com/raghudevopsb88/wealth-project/main/artifacts/${service_name}.tar.gz
+  cd /app
+  tar xzf /tmp/${service_name}.tar.gz
+}
+
+YC="\e[33m"
+RC="\e[31m"
+GC="\e[32m"
+NC="\e[0m"
+OUTPUT=/tmp/wmp.log
+rm -f $OUTPUT
+
+status_check() {
+  if [ $? -eq 0 ]; then
+    echo -e "${GC}SUCCESS${NC}"
+  else
+    echo -e "${RC}FAILURE${NC}"
+  fi
+}
--- frontend.sh
+++ frontend.sh
@@ -1,28 +1,42 @@
-dnf module disable nginx -y
-dnf module enable nginx:1.26 -y
-dnf install -y nginx
+source common.sh
 
-cp nginx.conf /etc/nginx/nginx.conf
+echo -e "${YC}Install Nginx${NC}"
+dnf module disable nginx -y &>>$OUTPUT
+dnf module enable nginx:1.26 -y &>>$OUTPUT
+dnf install -y nginx &>>$OUTPUT
+status_check
 
-systemctl enable nginx
-systemctl start nginx
+echo -e "${YC}Update Nginx Config${NC}"
+cp nginx.conf /etc/nginx/nginx.conf &>>$OUTPUT
+status_check
 
-curl -fsSL https://rpm.nodesource.com/setup_22.x | bash -
-dnf install -y nodejs
+echo -e "${YC}Install NodeJS${NC}"
+curl -fsSL https://rpm.nodesource.com/setup_22.x | bash - &>>$OUTPUT
+dnf install -y nodejs &>>$OUTPUT
 
-node --version
-npm --version
+node --version &>>$OUTPUT
+npm --version &>>$OUTPUT
+status_check
 
-curl -L -o /tmp/frontend.tar.gz https://raw.githubusercontent.com/raghudevopsb88/wealth-project/main/artifacts/frontend.tar.gz
-mkdir -p /tmp/frontend
+echo -e "${YC}Download and extract Application${NC}"
+curl -L -o /tmp/frontend.tar.gz https://raw.githubusercontent.com/raghudevopsb88/wealth-project/main/artifacts/frontend.tar.gz &>>$OUTPUT
+mkdir -p /tmp/frontend &>>$OUTPUT
 cd /tmp/frontend
-tar xzf /tmp/frontend.tar.gz
+tar xzf /tmp/frontend.tar.gz &>>$OUTPUT
+status_check
 
+echo -e "${YC}Build frontend code${NC}"
 cd /tmp/frontend
-npm ci
-npm run build
+npm ci &>>$OUTPUT
+npm run build &>>$OUTPUT
+status_check
 
-rm -rf /usr/share/nginx/html/*
-cp -r /tmp/frontend/dist/* /usr/share/nginx/html/
+echo -e "${YC}Update frontend code${NC}"
+rm -rf /usr/share/nginx/html/* &>>$OUTPUT
+cp -r /tmp/frontend/dist/* /usr/share/nginx/html/ &>>$OUTPUT
+status_check
 
-systemctl restart nginx
+echo -e "${YC}Start nginx service${NC}"
+systemctl enable nginx &>>$OUTPUT
+systemctl restart nginx &>>$OUTPUT
+status_check
