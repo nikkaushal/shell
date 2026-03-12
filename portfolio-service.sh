d7a22c99cef1531689f44ff22115b64a4a362f38
nikkaushal <nikkaushal@gmial.com>
2026-03-12 19:56:26 -0400

    update to the portfolio-service and common.sh file

--- portfolio-service.sh
+++ portfolio-service.sh
@@ -1,32 +1,28 @@
 source common.sh
 service_name=portfolio-service
 
-echo -e "${YC}Install java{NC}"
+echo -e "${YC}Install Java${NC}"
 dnf install -y java-21-openjdk-devel &>>$OUTPUT
 status_check
 
-echo -e "${YC}Update portfolio-service service file${NC}"
-cp ${service_name}.service /etc/systemd/system/${service_name}.service &>>$OUTPUT
-status_check
-
-echo -e "${YC}Prepare application prerequisites${NC}" 
+echo -e "${YC}Download and Extract Application${NC}"
 app_prereq
 status_check
 
-echo -e "${YC}Change user permissions${NC}"
+echo -e "${YC}Build Application${NC}"
 cd /app
 chmod +x gradlew &>>$OUTPUT
 ./gradlew bootJar --no-daemon -x test &>>$OUTPUT
 status_check
 
-echo -e "${YC}Copy jar files and change appuser permsisions${NC}"
+echo -e "${YC}Copy Jar File${NC}"
 cp /app/build/libs/*.jar /app/${service_name}.jar &>>$OUTPUT
-chown -R appuser:appuser /app &>>$OUTPUT
-chmod o-rwx /app -R &>>$OUTPUT
 status_check
 
-echo -e "${YC}Start portfolio-service${NC}"
-systemctl daemon-reload
-systemctl enable ${service_name}
-systemctl start ${service_name}
+set_permissions
+
+echo -e "${YC}Copy Service File${NC}"
+cp ${service_name}.service /etc/systemd/system/${service_name}.service &>>$OUTPUT
 status_check
+
+start_service
